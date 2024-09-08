c89863052888da49f45965125dcc089d1874aba1
dedevez <74338703+dedevez@users.noreply.github.com>
2024-09-07 20:47:42 -0400

    Create layout.jsx
    Created Layout template for each page (header-children-footer)

---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header_Navbar from "../header_navbar/Header_Navbar";
+import Footer from "../footer/Footer";
+import Header from "../header/Header";
+
+const Layout = ({children}) => {
+    return (
+        <>
+            <Header_Navbar/>
+            {children}
+            <Footer/>
+        </>
+    )
+}
+
+export default Layout;
