1c59e534ffb7d549d7959a4027eae3fe0a405453
dedevez <74338703+dedevez@users.noreply.github.com>
2024-09-07 20:51:45 -0400

    Created Home_page.jsx and updated App.jsx
    Created a homepage for the website for better navigation using React routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header_Navbar from "./components/header_navbar/Header_Navbar";
-import Header from "./components/header/Header";
-import Nav from "./components/nav/Nav";
-import About from "./components/about/About";
-import Experience from "./components/experience/Experience";
-import Portfolio from "./components/portfolio/Portfolio";
-import Blog from "./components/blog/Blog";
-import Specialties from "./components/specialties/Specialties";
-import Contact from "./components/contact/Contact";
+
 import Footer from "./components/footer/Footer";
 import Portfolio_Page from "./pages/portfolio_page/Portfolio_Page";
 import Blog_Page from "./pages/blog_page/Blog_Page";
@@ -20,14 +13,7 @@ const App = () => {
                 <Route path="/" element={
                     <>
                         <Header_Navbar/>
-                        <Header/>
-                        <Nav/>
-                        <About/>
-                        <Experience/>
-                        <Portfolio/>
-                        <Blog/>
-                        <Specialties/>
-                        <Contact/>
+
                         <Footer/>
                     </>
                 }/>
