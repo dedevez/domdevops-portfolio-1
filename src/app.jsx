a3f4a8f962e58c12f3d89062d624891f7818728a
dedevez <74338703+dedevez@users.noreply.github.com>
2024-09-12 14:10:19 -0400

    "Remove references to blog_page"

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom
 import Layout from "./components/layout/Layout";
 import Home_Page from "./pages/home_page/Home_Page";
 import Portfolio_Page from "./pages/portfolio_page/Portfolio_Page";
-import Blog_Page from "./pages/blog_page/Blog_Page";
 
 const App = () => {
     return (
--- src/pages/home_page/Home_Page.jsx
+++ src/pages/home_page/Home_Page.jsx
@@ -4,7 +4,6 @@ import Nav from "../../components/nav/Nav";
 import About from "../../components/about/About";
 import Experience from "../../components/experience/Experience";
 import Portfolio from "../../components/portfolio/Portfolio";
-import Blog from "../../components/blog/Blog";
 import Specialties from "../../components/specialties/Specialties";
 import Contact from "../../components/contact/Contact";
 
